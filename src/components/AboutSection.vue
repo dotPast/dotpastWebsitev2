2322768b3ed7f2c3d677b6ee1639656fda1b9e55
dotPast <83116565+dotPast@users.noreply.github.com>
2023-12-21 19:21:53 +0300

    add: Init Commit

---
+++ src/components/AboutSection.vue
@@ -0,0 +1,29 @@
+<script setup>
+import AboutItem from './AboutItem.vue'
+
+</script>
+
+<template>
+  <AboutItem style="margin-top: 1em;">
+    <template #icon>
+      
+    </template>
+    <template #heading>About</template>
+
+    heya! im dotPast or just dot (he/him).<br><br>
+    i am a minor (above twitch 13 years old requirement) from russia (not very proud of this) and i am a nerd. like i almost match the definition of nerd.<br><br>
+    i often stream minecraft but eventually i will stream development stuff (like making my own twitch bot and etc)
+  </AboutItem>
+  <AboutItem style="margin-top: 1em;">
+    <template #icon>
+      
+    </template>
+    <template #heading>Links</template>
+
+    <article>󰗃 - <a href="https://youtube.com/@dotpast" target="_blank">@dotpast</a> - YouTube. probably dead but that a later problem</article>
+    <article> - <a href="https://twitch.tv/dotpast" target="_blank">@dotpast</a> - Twitch. Definetly more alive than YouTube</article>
+    <article>󰫑 - <a href="https://mastodon.social/@dotpast" target="_blank">@dotpast</a> - Mastodon. Open-source Twitter i guess</article>
+    <article>󰕄 - <a href="https://twitter.com/ImDotPast" target="_blank">@ImDotPast</a> - Twitter. The bird app itself where i only read</article>
+  </AboutItem>
+
+</template>
