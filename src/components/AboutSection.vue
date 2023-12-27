071e46fe174a484ba3f492fcd3241b552670ab53
dotPast <83116565+dotPast@users.noreply.github.com>
2023-12-27 15:52:04 +0300

    fix: trying to fix mastodon verification

--- src/components/AboutSection.vue
+++ src/components/AboutSection.vue
@@ -23,7 +23,7 @@ import AboutItem from './AboutItem.vue'
     <article>󰗃 - <a href="https://youtube.com/@dotpast" target="_blank">@dotpast</a> - YouTube. Dead but that a later problem</article>
     <article> - <a href="https://twitch.tv/dotpast" target="_blank">@dotpast</a> - Twitch. Probably will be more alive than YouTube</article>
     <article>󰊤 - <a href="https://github.com/dotPast" target="_blank">dotPast</a> - GitHub. Even the source code of this thing is over there (feel free to roast my coding skills)</article>
-    <article>󰫑 - <a href="https://mas.to/@dotpast" target="_blank" rel="me">@dotpast@mas.to</a> - mas.to (Mastodon Instance). Open-source Twitter i guess</article>
+    <article>󰫑 - <a rel="me" href="https://mas.to/@dotpast">@dotpast@mas.to</a> - mas.to (Mastodon Instance). Open-source Twitter i guess</article>
     <article>󰕄 - <a href="https://twitter.com/ImDotPast" target="_blank">@ImDotPast</a> - Twitter. The bird app itself where i only read because yes</article>
 
   </AboutItem>
